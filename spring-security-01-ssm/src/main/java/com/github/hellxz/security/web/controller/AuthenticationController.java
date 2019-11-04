803cfaef4455e2d336242f4e5d1bdb7450dcda37
hellxz <hellxz001@foxmail.com>
2019-11-04 15:41:32 +0800

    添加自定义登录处理控制器

--- spring-security-01-ssm/src/main/java/com/github/hellxz/security/web/controller/AuthenticationController.java
+++ spring-security-01-ssm/src/main/java/com/github/hellxz/security/web/controller/AuthenticationController.java
@@ -1,15 +1,21 @@
 package com.github.hellxz.security.web.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping("user")
 public class AuthenticationController {
 
-    @RequestMapping("/loginPage")
+    @RequestMapping(value = "/user/loginPage", method = RequestMethod.GET)
     public String loginPage(){
         return "loginPage";
+    }
+
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST)
+    public String login(){
+        return "login";
     }
 }
--- spring-security-01-ssm/src/main/webapp/WEB-INF/views/loginPage.jsp
+++ spring-security-01-ssm/src/main/webapp/WEB-INF/views/loginPage.jsp
@@ -6,7 +6,7 @@
 </head>
 <body>
 <h3>登录页面</h3>
-<form action="${request.requestContext.requestPath}/login" method="post">
+<form action="/user/login" method="post">
 用户名：<input type="text" name="username"/><br/>
 密码：<input type="password" name="password"/><br/>
 <input type="submit" value="提交">
