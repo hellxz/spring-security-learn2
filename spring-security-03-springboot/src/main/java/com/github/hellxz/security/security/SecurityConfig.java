4891bb3aff77f31854d33096cd8568723b97203d
hellxz <hellxz001@foxmail.com>
2019-11-15 13:27:20 +0800

    添加安全配置类

--- spring-security-03-springboot/src/main/java/com/github/hellxz/security/security/SecurityConfig.java
+++ spring-security-03-springboot/src/main/java/com/github/hellxz/security/security/SecurityConfig.java
@@ -1,8 +1,24 @@
 package com.github.hellxz.security.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("hellxz").password("1234").authorities("ROLE_PRODUCT_ADD");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").fullyAuthenticated()
+                .and().httpBasic();
+    }
 }
