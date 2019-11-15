f68c0abdefe89b6ed396fbb6d1dbaa9809b8fddb
hellxz <hellxz001@foxmail.com>
2019-11-15 13:36:59 +0800

    使用表单登录，添加产品需要角色

--- spring-security-03-springboot/src/main/java/com/github/hellxz/security/security/SecurityConfig.java
+++ spring-security-03-springboot/src/main/java/com/github/hellxz/security/security/SecurityConfig.java
@@ -10,15 +10,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 代替security:authenticationManger
+     * @param auth
+     * @throws Exception
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("hellxz").password("1234").authorities("ROLE_PRODUCT_ADD");
+        auth.inMemoryAuthentication().withUser("hellxz").password("1234").authorities("ROLE_PRODUCT_ADD", "ROLE_PRODUCT_GET");
     }
 
+    /**
+     * 代替之前security:http配置
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/product/add").hasAuthority("ROLE_PRODUCT_ADD")
+                .antMatchers("/product/del").hasAuthority("ROLE_PRODUCT_DEL")
+                .antMatchers("/product/mod").hasAuthority("ROLE_PRODUCT_MOD")
+                .antMatchers("/product/get").hasAuthority("ROLE_PRODUCT_GET")
                 .antMatchers("/**").fullyAuthenticated()
-                .and().httpBasic();
+                .and().formLogin();
     }
 }
